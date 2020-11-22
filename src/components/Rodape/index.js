1c54a68eceaf28ea9f3f012b489d8d49ad3bbf89
Rodrigo Aramburu <rodrigoaramburu@gmail.com>
2020-11-22 09:23:26 -0300

    altecao rodape

--- src/components/Rodape/index.js
+++ src/components/Rodape/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-no-target-blank */
 import styled from "styled-components";
-
+import IconGithub from '../../assets/github.svg';
 
 
 const RodapeContainer = styled.footer`
@@ -34,24 +34,50 @@ const RodapeContainer = styled.footer`
         list-style-type: circle;
     }
 
+    .container-links{
+        display: flex;
+        align-items: baseline;
+        padding-top:20px;
+    }
+
+    .container-links div{
+        margin-right: 30px;
+    }
+
+    .github {
+        margin-left: auto
+    }
+    .github img{
+        vertical-align: middle;
+        margin-right: 10px;
+        filter: invert();
+    }
 
     @media(max-width: 640px){
         width: 100%;
     }
 `;
 
-function Rodape(){
+function Rodape() {
 
     return (
         <RodapeContainer>
             <div className="wrap">
-                <div className="links">
-                    <a href="https://retropunk.com.br/loja/91-four-against-darkness" target="_blank">Versão Brasileira</a>
-                    <h3>Mapas</h3>
-                    <ul>
-                        <li><a href="https://probabletrain.itch.io/dungeon-scrawl" target="_blank">Dungeon Scrawl</a></li>
-                    </ul>
+                <a href="https://retropunk.com.br/loja/91-four-against-darkness" target="_blank"> Four Agaist Darkness - Versão Brasileira</a>
+                <div className="container-links">
+                    <div className="links">
+                        <h4>Mapas</h4>
+                        <ul>
+                            <li><a href="https://probabletrain.itch.io/dungeon-scrawl" target="_blank">Dungeon Scrawl</a></li>
+                        </ul>
+                    </div>
                     
+                    <div className="github">
+                        <a href="https://github.com/rodrigoaramburu/4adficha" target="_blank">
+                            <img src={IconGithub} width="24" height="24" alt="Icone do Github" />
+                            Github do Projeto
+                        </a>
+                    </div>
                 </div>
             </div>
         </RodapeContainer>
